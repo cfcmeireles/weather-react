40ea430c00017507662fa02cb564c5a1d13fa3f5
cmeireles1 <carinacmeireles@gmail.com>
2021-02-18 19:32:41 +0000

    Changed github footer link

--- src/Footer.js
+++ src/Footer.js
@@ -3,7 +3,7 @@ import React from "react";
 export default function Footer() {
   return (
     <footer>
-      <a href="https://github.com/cmeireles1/weather-app" target="_blank">
+      <a href="https://github.com/cmeireles1/weather-react" target="_blank">
         Open-source code
       </a>{" "}
       by Carina Meireles
